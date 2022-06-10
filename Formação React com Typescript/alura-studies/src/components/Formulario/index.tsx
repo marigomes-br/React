0eac7eb97882dbcf653ddd681702e6828ebcf564
Mariana Gomes Santos <84389127+marigomes-br@users.noreply.github.com>
2022-06-10 16:14:13 -0300

    Iniciando com css modules

---
+++ Formação React com Typescript/alura-studies/src/components/Formulario/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Botao from "../Botao";
+import style from "./Formulario.module.scss";
+
+const Formulario = () => {
+  return (
+    <form className={style.novaTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          id="tarefa"
+          placeholder="O que quer estudar?"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Botao />
+    </form>
+  );
+};
+
+export default Formulario;
